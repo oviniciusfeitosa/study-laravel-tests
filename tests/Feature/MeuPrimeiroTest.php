aafedbd10d404d8c1016f131b641302238cb8c6a
vinnyfs89 <viniciusfesil@gmail.com>
2019-03-05 19:31:37 -0300

    [update] Adicionando mais testes

--- tests/Feature/MeuPrimeiroTest.php
+++ tests/Feature/MeuPrimeiroTest.php
@@ -24,6 +24,7 @@ class MeuPrimeiroTest extends TestCase
     public function testCaixaContemItem()
     {
         $caixa = new Caixa(['carro', 'mochila', 'garfo']);
+
         $this->assertTrue($caixa->contem('mochila'));
         $this->assertFalse($caixa->contem('cubo magico'));
     }
@@ -31,8 +32,25 @@ class MeuPrimeiroTest extends TestCase
     public function testCaixaContemUmItem()
     {
         $caixa = new Caixa(['lençol']);
+
         $this->assertEquals('lençol', $caixa->pegarUm());
+
         // Null, agora a caixa está vazia
         $this->assertNull($caixa->pegarUm());
+    }
+
+    public function testComecaComLetra()
+    {
+        $caixa = new Caixa(['cooler', 'mouse', 'fone', 'celular', 'computador']);
+
+        $results = $caixa->comecaCom('c');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('cooler', $results);
+        $this->assertContains('celular', $results);
+        $this->assertContains('computador', $results);
+
+        // Vai devolver um array vazio
+        $this->assertEmpty($caixa->comecaCom('s'));
     }
 }
